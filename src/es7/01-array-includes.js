304534863c984ddf4a94eb7c3da0456d87a23afd
gustavojuan <gustavojuan@gmail.com>
2023-03-09 06:00:45 +0100

    array includes explained in comnmentsd

--- src/es7/01-array-includes.js
+++ src/es7/01-array-includes.js
@@ -1,5 +1,5 @@
 let numbers = [1,2,3,4,5];
-console.log(numbers.includes(5));
+console.log(numbers.includes(5));//true
 
-const names = ['oscar','ana','ulises'];
-console.log(names.includes('ana'));
+const names = ['Oscar','Ana','Ulises'];
+console.log(names.includes('ana')); //false
