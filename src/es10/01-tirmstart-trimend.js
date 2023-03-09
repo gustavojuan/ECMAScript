49d5d944a3ad2ac1c3572d52440fa183177982c7
gustavojuan <gustavojuan@gmail.com>
2023-03-09 15:50:56 +0100

    Typo

--- src/es10/01-tirmstart-trimend.js
+++
@@ -1,4 +0,0 @@
-const hello  = "     Hello!      ";
-
-console.log(hello.trimStart()); //Hello!....
-console.log(hello.trimEnd()); //     Hello!
---
+++ src/es10/01-trimstart-trimend.js
@@ -0,0 +1,4 @@
+const hello  = "     Hello!      ";
+
+console.log(hello.trimStart()); //Hello!....
+console.log(hello.trimEnd()); //     Hello!
